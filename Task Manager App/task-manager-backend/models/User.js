140b224c5458ee713bc1e7bf174f548d4e27f17f
av <106282443+avthind@users.noreply.github.com>
2025-06-30 15:59:22 -0700

    Delete directory

--- Task Manager App/task-manager-backend/models/User.js
+++
@@ -1,7 +0,0 @@
-// models/User.js
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: { type: String }
-});
-module.exports = mongoose.model('User', UserSchema);
--- Task Manager App/task-manager-frontend/src/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-// ProtectedRoute.js
-import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "./utils/auth";
-
-export default function ProtectedRoute({ children }) {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
-}
